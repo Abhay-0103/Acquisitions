22178a7809c5c7f6bcb4657363161847eb3e9cc6
Abhay-0103 <ab0321054@gmail.com>
2025-09-29 16:55:47 +0530

    Implement ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello Bhai Meh Acquisitions')
-})
+  res.status(200).send('Hello Bhai Meh Acquisitions');
+});
 
 export default app;
