f128bd973805e9dd525b4ae4475d969ff499e6ce
Abhay-0103 <ab0321054@gmail.com>
2025-10-02 01:59:43 +0530

    added api rotute for testing

--- src/app.js
+++ src/app.js
@@ -35,6 +35,10 @@ app.get('/api', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);  //api.auth
-app.use('/api/users', usersRoutes)
+app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route Not Found' });
+})
 
 export default app;
